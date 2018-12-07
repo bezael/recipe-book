e448b913c0323ebc6dd6f57ffbf371a235200a10
Viktor Borisov <viktor.borisov@knab.nl>
2018-12-07 10:45:18 +0200

    the new httpClient module implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { RecipeService } from './recipes/recipe.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -17,7 +18,6 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -37,7 +37,7 @@ import { HttpModule } from '@angular/http';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService],
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,23 +2,22 @@ import { map } from 'rxjs/operators';
 import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private httpClient: HttpClient, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    return this.http.put(
+    return this.httpClient.put(
       'https://ng-recipe-book-dfe7d.firebaseio.com/recipes.json',
       this.recipeService.getRecipes()
     );
   }
   getRecipes() {
-    this.http
-      .get('https://ng-recipe-book-dfe7d.firebaseio.com/recipes.json')
-      .pipe(map((response: Response) => {
-        const recipes: Recipe[] = response.json();
+    this.httpClient
+      .get<Recipe[]>('https://ng-recipe-book-dfe7d.firebaseio.com/recipes.json')
+      .pipe(map((recipes) => {
         for (const recipe of recipes) {
           if (!recipe.ingredients) {
             recipe.ingredients = [];
