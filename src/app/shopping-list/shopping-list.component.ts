5265db2f6641ccce9f95bbd476353fb2f38e8d80
Viktor Borisov <viktor.borisov@knab.nl>
2018-12-14 09:44:21 +0200

    fix for duplicate DI

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
-import { Ingredient } from '../shared/ingredient.model';
 import * as ShoppingListActions from './store/shopping-list.actions';
 import * as fromApp from '../store/app.reducers';
 
